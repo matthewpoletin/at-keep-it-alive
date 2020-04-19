2e67c01d06ac07922b163d451e747ade1226f1e3
Matvey Poletin <poletinm@yandex.ru>
2020-04-19 10:41:16 +0300

    Impression widget draft layout

--- Assets/Scripts/Ui/ImpressionWidget.cs
+++ Assets/Scripts/Ui/ImpressionWidget.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace KnowCrow.AT.KeepItAlive
 {
     public class ImpressionWidget : BaseView
     {
         [SerializeField] private VerticalProgressBar _verticalProgressBar = null;
+        [SerializeField] private TextMeshProUGUI _text = null;
 
         private ImpressionModel _impressionModel;
 
@@ -18,6 +20,7 @@ namespace KnowCrow.AT.KeepItAlive
         private void OnImpressionLevelChanged(float impressionLevel)
         {
             _verticalProgressBar.SetProgress(impressionLevel);
+            _text.text = $"{(int) (impressionLevel * 100)} / 100";
         }
 
         public override void Dispose()
